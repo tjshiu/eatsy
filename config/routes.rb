25d3fb55239bd39d5a395d7ae1a181b152eac2cf
tjshiu <tiffany.shiu.2@gmail.com>
2018-04-03 16:08:05 -0700

    Added a static pages controller and created a main with the id of root for the my react to find it

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json} do
-    resources :users [:index, :show, :create, :destroy, :update, :edit]
+  namespace :api, defaults: {format: :json} do
+    resources :user, only: [:create, :show, :new, :destroy, :update, :edit]
   end
 
-  root 'static_pages#root'
+  root "static_pages#root"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
