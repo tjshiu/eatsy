3446540dcb151193b60789bc9eaf8045d4d097f2
tjshiu <tiffany.shiu.2@gmail.com>
2018-04-03 15:42:05 -0700

    UPdated the entry file with react and react-dom

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'users/show'
+  namespace :api, defaults: { format: :json} do
+    resources :users [:index, :show, :create, :destroy, :update, :edit]
   end
 
-  namespace :api do
-    get 'users/create'
-  end
-
-  namespace :api do
-    get 'users/new'
-  end
-
-  namespace :api do
-    get 'users/destroy'
-  end
-
-  namespace :api do
-    get 'users/update'
-  end
-
-  namespace :api do
-    get 'users/edit'
-  end
+  root 'static_pages#root'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
