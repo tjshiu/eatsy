3e866268662cb54a075b5fd9d5a1e1243260f01f
tjshiu <tiffany.shiu.2@gmail.com>
2018-04-03 15:34:41 -0700

    Created Api::Users Controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::UsersController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
