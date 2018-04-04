25d6a73790d85c152ae2c3b16b4ccd12f6d1f1cc
tjshiu <tiffany.shiu.2@gmail.com>
2018-04-04 10:12:55 -0700

    Created the session form to register and sign in

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBarContainer from "./fixed/nav_bar_container";
+import RegisterFormContainer from "./session_form/register_form_container";
+import SignInFormContainer from "./session_form/sign_in_form_container";
 
 const App = () => (
   <div>
-    <NavBarContainer />
+    <header>
+      <NavBarContainer />
+    </header>
+
+    <Route path="/signin" component={SignInFormContainer} />
+    <Route path="/register" component={RegisterFormContainer} />
   </div>
 );
 
