369c94d5acce52d37074b72ae6bcd0ecbffd0f10
tjshiu <tiffany.shiu.2@gmail.com>
2018-04-05 13:42:43 -0700

    ensured that everything worked with the session form

--- frontend/components/session_form/sign_in_form.jsx
+++ frontend/components/session_form/sign_in_form.jsx
@@ -22,7 +22,7 @@ class SignInForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     const user = Object.assign({}, this.state);
-    this.props.processForm(user).then(() => this.props.closeModal());
+    this.props.processForm(user).then((user) => this.props.closeModal());
   }
 
   renderErrors() {
