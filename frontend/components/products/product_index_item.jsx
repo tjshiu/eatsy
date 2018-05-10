b5a353baa459a19e997b3c7b6793fdd6650a6caa
tjshiu <tiffany.shiu.2@gmail.com>
2018-05-10 12:05:06 -0700

    Added box shadow for user's show page

--- frontend/components/products/product_index_item.jsx
+++ frontend/components/products/product_index_item.jsx
@@ -10,7 +10,6 @@ const ProductIndexItem = ({ product }) => {
         <li><Link to={`/products/${product.id}`}><img className='index productImg' src={product.imageUrl}/></Link></li>
         <li className='index productName'><Link to={`/products/${product.id}`} className="index productName">{product.productName}</Link></li>
         <li><Link className="index productSeller" to={`/users/${product.seller.id}`}>{product.seller.username}</Link></li>
-        <li className="index productRating">Rating</li>
         <li className='index productCost'>${(product.cost).toFixed(2)}</li>
       </ul>
     </li>);
