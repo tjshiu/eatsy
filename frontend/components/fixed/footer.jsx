1973e790f235ea4c8e5862646aa694e22a1981c8
tjshiu <tiffany.shiu.2@gmail.com>
2018-05-10 21:10:10 -0700

    Added tags to the footer to link to my linkedIn and Github

--- frontend/components/fixed/footer.jsx
+++ frontend/components/fixed/footer.jsx
@@ -12,10 +12,19 @@ class NavBar extends React.Component {
         <nav className="Footer">
           <div className="FooterLeft">
             <Link to="/" className="header-link">
-              <h1 className="Logo">Eatsy</h1>
+              <h1 className="FooterLogo">Eatsy</h1>
             </Link>
+            <p>We make it easy to find your food</p>
           </div>
-          <div className="FooterRight" />
+          <div className="FooterRight" >
+            <div><i className="far fa-copyright"></i> 2018 Eatsy, Inc.</div>
+            <a className='tiffany-links' href="https://github.com/tjshiu/eatsy">
+              <img src="https://res.cloudinary.com/dwanjkcku/image/upload/v1526010598/GitHub-Mark-120px-plus.png" />
+            </a>
+            <a className='tiffany-links' href="https://www.linkedin.com/in/tiffany-shiu/">
+              <img src="https://res.cloudinary.com/dwanjkcku/image/upload/v1526010598/linkedin.png" />
+            </a>
+          </div>
         </nav>
       </div>
     );
