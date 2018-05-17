612f10acb0aaaa11407623d05c03194c1a6947f0
tjshiu <tiffany.shiu.2@gmail.com>
2018-05-17 11:28:43 -0700

    Fixed a bug due to rerendering of the page when state changed

--- frontend/components/homepage/homepage_index.jsx
+++ frontend/components/homepage/homepage_index.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import HomePageIndexItem from './homepage_index_item';
 
 class ProductIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: []};
+  }
+
 
   componentDidMount() {
-    this.props.fetchProducts();
+    this.props.fetchProducts().then(() =>
+      this.setState({ products: this.shuffle(this.props.products.slice(0, 6))})
+    );
   }
 
   shuffle(array) {
@@ -22,7 +29,7 @@ class ProductIndex extends React.Component {
   }
 
   render() {
-    const products = this.shuffle(this.props.products).slice(0, 6).map(product => {
+    const products = this.state.products.map(product => {
       return (
         <HomePageIndexItem
           key={`home-product-${product.id}`}
